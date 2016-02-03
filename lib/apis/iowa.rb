7d9b6bb9c0d9d247704e3ec0f88c31c7b96c280d
Andrew Schultz <as173171@gmail.com>
2016-02-03 16:31:52 -0500

    added an API, no big deal, thanks bradley for the help

---
+++ lib/apis/iowa.rb
@@ -0,0 +1,35 @@
+require 'net/http'
+require 'json'
+
+module Iowa
+	class Client
+
+		def initialize
+			@uri = 'https://www.kimonolabs.com/api/'
+		end
+
+		def make_request(endpoint)
+			JSON.parse(Net::HTTP.get(URI(@uri + endpoint.to_s)))
+		end
+
+		def dem
+		  return make_request('9wmpnk9k?apikey=tj1hdT1m9XSPUeh1LyEyBn7Q7mC6qIDg')
+		end
+
+		def rep
+		  return make_request('bj8hlwos?apikey=tj1hdT1m9XSPUeh1LyEyBn7Q7mC6qIDg')
+		end
+
+		def clin
+			thing = dem
+			return thing['results']['collection1'][1]['candidate']
+		end
+
+	end
+end
+
+
+
+# candidate = Iowa::Client.new
+# put candidate.clin => CLIN_NOM
+# put candidate.rep
