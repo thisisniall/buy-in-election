c1603d78f365d270fe9022cc7885ae823026fc23
Niall <niallpatrickmurphy@gmail.com>
2016-02-08 11:42:59 -0500

    modifications made to transactions controller to enable eventual buy/sell

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,7 @@ class Transaction < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :candidate
 
-	validates_presence_of :user, on: :create
-	validates_presence_of :candidate, on: :create
+# hashed out these validations for testing purposes
+	# validates_presence_of :user, on: :create
+	# validates_presence_of :candidate, on: :create
 end
